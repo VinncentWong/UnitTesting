3d9dc129d4e2b6e0d161738bb5f2319a1ce2a6c4
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-05-23 16:49:11 +0700

    use mockito

--- src/main/java/com/BusinessLogic.java
+++ src/main/java/com/BusinessLogic.java
@@ -8,4 +8,8 @@ public class BusinessLogic {
         }
         return sum;
     }
+
+    public void getData(){
+        
+    }
 }
--- src/test/java/com/TestBusinessLogic.java
+++ src/test/java/com/TestBusinessLogic.java
@@ -1,6 +1,8 @@
 package com;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,4 +15,16 @@ public class TestBusinessLogic {
         int expected = 15;
         assertEquals(expected, actual);
     }
+
+    // atau
+
+    @Test
+    public void checkIfCalculateSumIsTrueUsingMock(){
+        BusinessLogic logic = new BusinessLogic();
+        ILogin retrieveData = mock(ILogin.class);
+        when(retrieveData.getSomeData()).thenReturn(new int[] {1,2,3,4,5});
+        int expected = 15;
+        assertEquals(expected, logic.calculateSumArray(retrieveData.getSomeData()));
+    }
+
 }
