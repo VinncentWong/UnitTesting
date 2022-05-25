52bb036a4ff7e9f83c0c149d66fd6055511cf5bd
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-05-25 14:35:43 +0700

    test controller

---
+++ src/main/java/com/Controller.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    
+    @GetMapping("/hello")
+    public String getHelloWorld(){
+        return "Hello World";
+    }
+    
+}
