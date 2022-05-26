92ed0b43986dbfb4fceadc901f7d24abc84b7494
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-05-26 09:46:35 +0700

    use spring data jpa

--- src/main/java/com/entity/Item.java
+++ src/main/java/com/entity/Item.java
@@ -1,29 +1,46 @@
 package com.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private int stock;
+
     public Item(Long id, String name, int stock) {
         this.id = id;
         this.name = name;
         this.stock = stock;
     }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+    
     public void setName(String name) {
         this.name = name;
     }
+
     public int getStock() {
         return stock;
     }
+    
     public void setStock(int stock) {
         this.stock = stock;
     }
